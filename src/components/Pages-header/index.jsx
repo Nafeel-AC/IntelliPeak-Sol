ddd38290ee7245dd730181b4925a66849060277a
Muhammad Awais Abdullah <143254491+Awadul@users.noreply.github.com>
2025-08-22 19:35:58 +0500

    changed about video

--- src/components/Pages-header/index.jsx
+++ src/components/Pages-header/index.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 const PagesHeader = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(() => {});
+    }
+  }, []);
+
   return (
     <header className="pages-header circle-bg valign">
       <div className="container">
@@ -16,7 +24,14 @@ const PagesHeader = () => {
           </div>
           <div className="col-lg-10">
             <div className="img">
-              <img src="/img/slid/about.jpg" alt="" />
+              {/* <img src="/img/slid/about.jpg" alt="" /> */}
+              <video
+                ref={videoRef}
+                src="/img/slid/about.mp4"
+                muted
+                playsInline
+                className="responsive-header-video"
+              ></video>
             </div>
           </div>
         </div>
@@ -30,5 +45,30 @@ const PagesHeader = () => {
     </header>
   );
 };
+
+
+// Add styles for responsive video
+// You can move this to a CSS/SCSS file if preferred
+const style = `
+.responsive-header-video {
+  width: 100%;
+  max-width: 1000px;
+  height: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+@media (max-width: 1300px) {
+  .responsive-header-video {
+    max-width: 80vw;
+  }
+}
+`;
+
+if (typeof window !== 'undefined') {
+  const styleTag = document.createElement('style');
+  styleTag.innerHTML = style;
+  document.head.appendChild(styleTag);
+}
 
 export default PagesHeader;
